0cf3c01c3eae328824d175cce4b074e59c25add5
Thiago Pereira de Oliveira <1494349@sga.pucminas.br>
2025-04-03 08:36:42 -0300

    !teste

--- codigo/calc.c
+++ codigo/calc.c
@@ -12,6 +12,7 @@
 ////////////////////////////////////////////////
 // DEFINIÇÕES
 
+#define pi 3.14159265358979323846264338327950
 typedef double (*funcao) (double);
 
 ////////////////////////////////////////////////
@@ -31,7 +32,7 @@ int potencia (int a, int b)
 
 double integrar (funcao f, int nivelDePrecisao, double a, double b)
 {
-    long long N = 1000000000; // 10000000
+    long long N = 100000000; // 10000000
 
     double deltaX = (b - a) / N; // Largura que será usada nos retângulos
 
@@ -47,22 +48,27 @@ double integrar (funcao f, int nivelDePrecisao, double a, double b)
     return soma;
 }
 
+double imprimirIntegral (funcao f, double a, double b)
+{
+    static int q = 1;
+    printf ("Integral de %g ate %g da questao %d: %lf\n", a, b, q++, integrar (f, 0, a, b));
+}
+
 ////////////////////////////////////////////////
 // FUNÇÕES
 
-double funcao_1 (double x) { return (x + x*x);}   
-double funcao_2 (double x) { return (x);}   
-double funcao_3 (double x) { return (x*x);}   
-double funcao_4 (double x) { return (16/(sqrt (16 - x*x)));}
+double funcao_1 (double x) { return (4 * sqrt (36 - x * x)) ;} 
+double funcao_2 (double x) { return (2 * pi * (9  - x * x)) ;} 
+double funcao_3 (double x) { return (16/(sqrt (16 - x * x)));} 
 
 ////////////////////////////////////////////////
 // MAIN
 
 int main (void)
 {
-    double a = 0, b = 4;
-
-    printf ("Integral da funcao 4 entre '%g' e '%g': %g\n", a, b, integrar (funcao_4, 7, a, b));
+    imprimirIntegral (funcao_1, 0, 6);
+    imprimirIntegral (funcao_2, 0, 3);
+    imprimirIntegral (funcao_3, 0, 4);
 
     return 0;
 }
